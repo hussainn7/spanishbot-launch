99ac5abd7319522c1f3e896761da2277428b3498
Hussain <133125309+hussainn7@users.noreply.github.com>
2024-06-30 17:38:32 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 TOKEN = "7145390741:AAFnkpGf-Mv01_DflPWmpb403zq1SsfaBnA"
 
-PRICE = '950.00'
+price = 950.0
 
 information_about_company = 'We are wery good company, for exmpl'
 
-# FOR YooMoney
 # ACCOUNT_ID = 393037
 # SECRET_KEY = 'test_b9DiYPsa4a6kV-YyfI4QM0CtiO_65o51x4946MujKAo'
 
+
+# ВОТ ТУТ ТЕ САМЫЕ АДМИНЫ
+ADMIN_USER_ID = 1262676599
 
 
